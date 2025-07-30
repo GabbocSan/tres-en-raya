4d31b43ec5ff6a79581ffdb5b1ec7fcc0034c976
GabbocSan <gabocruzsanabria@gmail.com>
2025-07-30 11:21:24 -0400

    Se agrego la funcionalida de volver a turnos anteriores

--- src/Board.jsx
+++ src/Board.jsx
@@ -72,14 +72,35 @@ function calculateWinner(squares){
 }
 
 export default function Game(){
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[history.length-1];
+    const [currentMove, setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove];
+    const xIsNext = currentMove % 2 === 0; 
 
     function handlePlay(nextSquares){
-        setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
     }
+
+    function jumpTo(nextMove){
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((squares, move)=>{
+        let description;
+        if(move>0){
+            description = 'Ir al movimiento #'+move;
+        }
+        else{
+            description = 'Ir al inicio del juego';
+        }
+        return(
+            <li key={move}>
+                <button onClick={()=>jumpTo(move)}>{description}</button>
+            </li>
+        );
+    });
 
     return(
         <div className="game">
@@ -87,7 +108,7 @@ export default function Game(){
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
             </div>
             <div className="game-info">
-                <ol>{/*TODO*/}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
